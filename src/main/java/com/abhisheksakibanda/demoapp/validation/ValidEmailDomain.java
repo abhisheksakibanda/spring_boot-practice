503788ddbd839c6e4de3eae178cfb79190bd25fe
abhisheksakibanda <abhisheksakibanda@gmail.com>
2025-01-31 04:01:29 -0500

    added necessary annotations

--- src/main/java/com/abhisheksakibanda/demoapp/validation/ValidEmailDomain.java
+++ src/main/java/com/abhisheksakibanda/demoapp/validation/ValidEmailDomain.java
@@ -1,9 +1,18 @@
 package com.abhisheksakibanda.demoapp.validation;
 
+import jakarta.validation.Constraint;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import jakarta.validation.Payload;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = ValidEmailDomainValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEmailDomain {
     String message() default "Email domain must end with '.edu'";
 
@@ -15,7 +24,7 @@ public @interface ValidEmailDomain {
 class ValidEmailDomainValidator implements ConstraintValidator<ValidEmailDomain, String> {
 
     @Override
-        public void initialize(ValidEmailDomain validEmailDomain) {
+    public void initialize(ValidEmailDomain validEmailDomain) {
         ConstraintValidator.super.initialize(validEmailDomain);
     }
 
